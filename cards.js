96f07d13b722e7b9df7c9d89e191d3257831c31e
Adam Smith <hagansmith@gmail.com>
2017-08-19 14:02:23 -0500

    updated code for cards.js

--- cards.js
+++ cards.js
@@ -6,26 +6,27 @@ var cardNum = 0;
 	
 //1. When the user enters in text into the text area and then clicks the create button, 
 create.addEventListener('click',function(){	
-	domString(text);
-	delListener();
+	writeToArray(text.value);
+	dynamicIds();
+	deleteBtn();
 })
+
 //create a new card element in the DOM that includes it's own delete button.
-function domString(card) {	
-	cardNum + 1;
+function domString() {	
+	for (var i = 0; i < cardArray.length; i++) {
+	cardNum = i;
 	var cardString = '';
-	cardString += `<div class="cardElement" id="card_${cardNum}">`
-	cardString += `<p> ${card.value} </p>`
-	cardString += `<button type="button" class="btn" id="delBtn_${cardNum}">Delete</button>`
+	cardString += `<div class="cardElement" id="card_${i}">`
+	cardString += `<p> ${cardArray[i]} </p>`
+	cardString += `<button type="button" class="btn" id="delBtn_${i}">Delete</button>`
 	cardString += `</div>`
-	cardNum ++
-	writeToArray(cardString);
-	return cardNum;
+	writeToDom(cardString);
+	}	
 }
 
-
 function writeToArray(string) {
-	cardArray += string
-	writeToDom(string)
+	cardArray.push(string)
+	domString();
 }
 
 function writeToDom(string) {
@@ -34,13 +35,21 @@ function writeToDom(string) {
 
 //2. When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. 
 	//Not just made invisible, actually removed from the DOM.
-function delListener() {
+function dynamicIds() {
+	for(var j=0; j < cardArray.length; j++){
 	//These variables must be defined as a part of the fuction as they dont exist at the page load and need to be executed only after the DOM string has been created.
-	var cardElement = document.getElementsById('cardElement');
-	console.log('value of the cardElement:', cardElement);
-	var delCard = document.getElementsById('btn');
-	console.log('value of the delCard:', delCard);
-	delCard.addEventListener('click', function(){
- 		card.removeChild(cardElement);
- 	})
+	// console.log('card value in del Listener', cardNum);
+	// console.log('value of the cardElement:', cardElement);
+	var delCard = document.getElementById(`delBtn_${j}`);
+	// console.log('value of the delCard:', delCard);
+	// delCard.addEventListener('click', function(){	
  }
+ delCard.addEventListener('click', deleteBtn);
+}
+
+function deleteBtn(e) {
+console.log(e);
+	card.removeChild(e.target.parentNode);
+	
+};
+
